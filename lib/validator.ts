476c1194bb32e4a82a7ecac6c38730647b2edaf7
CristianB <angelC.bV@outlpook.com>
2024-02-06 13:09:01 -0600

    description characters extended

--- lib/validator.ts
+++ lib/validator.ts
@@ -2,7 +2,7 @@ import * as z  from "zod";
 
 export const postFormSchema = z.object({
     title: z.string().min(2, 'Title most be at least 2 characters'),
-    description: z.string().min(20, 'Desciption most be at least 20 characters').max(850,'Desciption most be less than 850 characters'),
+    description: z.string().min(20, 'Desciption most be at least 20 characters').max(2000,'Desciption most be less than 2000 characters'),
     private: z.boolean(),
     imageUrl: z.string(),
     categoryId: z.string(),
